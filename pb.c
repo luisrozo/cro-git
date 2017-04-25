ec42cb3dd814b9887a0238a7917fc015f30663cf
luisrozo <luis.rozobueno@alum.uca.es>
2017-04-25 15:30:13 +0200

    Arreglando error ortográfico a inglés británico

--- pb.c
+++ pb.c
@@ -44,7 +44,7 @@ int main(int argc, char** argv)
 {
         int balls[6];
     int count_balls = 0;
-    int favorite = 0; // this should be a bool
+    int favourite = 0; // this should be a bool
     for (int i=1; i<argc; i++)
     {
         goto usage_error;
@@ -86,14 +86,14 @@ int main(int argc, char** argv)
         {
             result = result * 2;
         }
-        if (favorite)
+        if (favourite)
         {
             result = result * 2;
         }
         printf("%d percent chance of winning\n", result);
         return 0;
 usage_error:
-fprintf(stderr, "Usage: %s [-favorite] (5 white balls)
+fprintf(stderr, "Usage: %s [-favourite] (5 white balls)
 power_ball\n", argv[0]);
 return -1;
 }
