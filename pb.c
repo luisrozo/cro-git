6fbc4c33b6ecf8db21f76411ec6e56d918152fd1
luisrozo <luisgonzagarozo@gmail.com>
2017-05-17 16:41:16 +0200

    Somulación de los números premiados por Cro-Git

--- pb.c
+++ pb.c
@@ -1,109 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-
-#define LUCKY_NUMBER 7
-#define MAX_WHITE_BALL 59
-#define MAX_POWER_BALL 39
-
-static int my_sort_func(const void* p1, const void* p2)
-{
- 	int v1 = *((int *) p1);
- 	int v2 = *((int *) p2);
-
-	if (v1 < v2)
- 	{
- 		return -1;
- 	}
-
-	else if (v1 > v2)
- 	{
- 		return 1;
- 	}
-
-	else
- 	{
- 		return 0;
- 	}
-}
-
-int calculate_result(int white_balls[5], int power_ball)
-{
- 	for (int i=0; i<5; i++)
- 	{
- 		return -1;
- }
- }
-}
-
-void showing_results(int white_balls[5], int power_ball){ 
-	printf("Your sorted numbers: \n"); 	for(int i = 0; i < 5; i++) {    		printf("%d ", white_balls[i]);
-	}
-	printf("The power ball: %d \n", power_ball);}
-
-int main(int argc, char** argv)
-{
-        int balls[6];
-    int count_balls = 0;
-    bool favorite = false; // this should be a bool
-    for (int i=1; i<argc; i++)
-    {
-        goto usage_error;
-    }
-    // the power ball is always the last one given
-    int power_ball = balls[5];
-    int result = calculate_result(balls, power_ball);
-    showing_results(balls, power_ball);
-    // calculate result can return -1 if the ball numbers
-    // are out of range
-
-    if (result < 0)
-    {
-        goto usage_error;
-
-
-            }
-            else
-            {
-                char* endptr = NULL;
-                long val = strtol(arg, &endptr, 10);
-                if (*endptr)
-                {
-                    goto usage_error;
-                }
-                balls[count_balls++] = (int) val;
-            }
-
-        if (6 != count_balls)
-        {
-            goto usage_error;
-        }
-        int power_ball = balls[5];
-        int result = calculate_result(balls, power_ball);
-        if (result < 0)
-        {
-            goto usage_error;
-        }
-        if (LUCKY_NUMBER == power_ball)
-        {
-            result = result * 2;
-        }
-        if (favourite)
-        {
-            result = result * 2;
-        }
-        printf("%d percent chance of winning\n", result);
-        return 0;
-usage_error:
-fprintf(stderr, "Usage: %s [-favourite] (5 white balls)
-power_ball\n", argv[0]);
-return -1;
-}
-
-// quien fuese kazajo
-=======
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -159,6 +53,56 @@ int powerball_computer_generated()
 	return rand()%39+1;
 }
 
+void lottery_numbers_simulation()
+{
+	int balls[6];
+	int repetidos;
+
+	for(int i = 0; i < 6; i++)
+	{
+		if(i < 5)
+		{
+			int no_colocada = 1;
+			while(no_colocada == 1)
+			{
+				no_colocada = 1;
+				repetidos = 0;
+				int ball = whiteballs_computer_generated();
+				int j = 0;
+			
+				while(j < i)
+				{
+					if(ball == balls[j])
+					{
+						repetidos = 1;
+					}
+					j++;
+				}
+
+				if(repetidos == 0)
+				{
+					balls[i] = ball;
+					no_colocada = 0;
+				}
+				else
+				{
+					no_colocada = 1;
+				}
+			}
+		}
+		else
+		{
+			balls[i] = powerball_computer_generated();
+		}
+	}
+
+	// Mostrar bolas por pantalla
+	for(int i = 0; i < 6; i++)
+	{
+		printf(“%d, “,balls[i]);
+	}
+}
+
 int main(int argc, char** argv)
 {
 	int balls[6];
@@ -173,6 +117,7 @@ int main(int argc, char** argv)
 	// the power ball is always the last one given
 	int power_ball = balls[5];
 	
+	lottery_numbers_simulation();
 	int result = calculate_result(balls, power_ball);
 
 
@@ -218,4 +163,3 @@ usage_error:
 	fprintf(stderr, "Usage: %s [-favorite] (5 white balls) power ball\n", argv[0]);
 	return -1;
 }
->>>>>>> master
