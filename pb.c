986e111765f70997b36ad46ee90dc6897e52d866
Antonio Vélez Estévez <antonio.velezestevez@alum.uca.es>
2017-05-17 18:39:00 +0200

    Modificaciones en la entrada de la función lottery_numbers_simulation y adaptando la función calculate_result para calcular probabilidades

--- pb.c
+++ pb.c
@@ -26,14 +26,14 @@ static int my_sort_func(const void* p1, const void* p2)
     }
 }
 
-int calculate_result(int white_balls[5], int power_ball)
+int calculate_result(int white_balls[6])
 {
   for (int i=0; i<5; i++)
     {
       if ((white_balls[i] < 1) || (white_balls[i] > MAX_WHITE_BALL))
-	{
-	  return -1;
-	}
+        {
+          return -1;
+        }
     }
 
   // lottery ball numbers are always shown sorted
@@ -50,7 +50,7 @@ int whiteballs_computer_generated()
 
 int powerball_computer_generated()
 {
-	return rand()%39+1;
+	return rand()%MAX_POWER_BALL+1;
 }
 
 void checkwhiteballs(int balls[5], int control)
@@ -65,6 +65,22 @@ void checkwhiteballs(int balls[5], int control)
   }
 }
 
+void lottery_numbers_simulation(int balls[6]){
+
+	for(int i = 0; i < 6; i++){
+
+		if(i < 5){
+			balls[i] = whiteballs_computer_generated();
+			checkwhiteballs(balls, i);
+		}else
+			balls[i] = powerball_computer_generated();
+	}
+
+	for(int i = 0; i < 6; i++){
+		printf("%d",balls[i]);
+	}
+}
+
 int main(int argc, char** argv)
 {
 	int balls[6];
@@ -72,56 +88,58 @@ int main(int argc, char** argv)
   int favorite = 0; //this should be a bool
 
 	for (int i=1; i <argc; i++)
-	{
-		goto usage_error;
-	}
+    {
+      goto usage_error;
+    }
 
 	// the power ball is always the last one given
 	int power_ball = balls[5];
-	
-	int result = calculate_result(balls, power_ball);
 
+  printf("\n--- The lottery numbers ---\n");
+	lottery_numbers_simulation(lott);
+	int result = calculate_result(balls, power_ball);
+  showing_results(balls, power_ball);
 
 	// calculate result can return -1 if the ball numbers
 	// are out of range
 
 	if (result < 0)
-	{
-		goto usage_error;
-	}
+    {
+      goto usage_error;
+    }
 
 	if (6 != count_balls)
-        {
-            for (int i = 0; i < 5; i++){
-		balls[i] = whiteballs_computer_generated();
-    checkwhiteballs(balls, i);
-	    }
-		balls[5] = powerball_computer_generated(); // Power ball
-		printf("Your numbers are: ");
+    {
+      balls[i] = whiteballs_computer_generated();
+      checkwhiteballs(balls, i);
+    }
+}
+balls[5] = powerball_computer_generated(); // Power ball
+printf("Your numbers are: ");
 		
-		  for (int i = 0; i < 5; i++){
-			printf("%d ", balls[i]);
-		  }
+for (int i = 0; i < 5; i++){
+  printf("%d ", balls[i]);
+ }
 		
-		printf("\nAnd the power ball:");
-		printf(" %d\n", balls[5]);
-        }
+printf("\nAnd the power ball:");
+printf(" %d\n", balls[5]);
+}
 
-	if (LUCKY_NUMBER == power_ball)
+if (LUCKY_NUMBER == power_ball)
 	{
 		result = result * 2;
 	}
 
-	if (favorite)
+if (favorite)
 	{
 		result = result * 2;
 	}
 
-	printf("%d percent chance of winning\n", result);
+printf("%d percent chance of winning\n", result);
 
-	return 0;
+return 0;
 
 usage_error:
-	fprintf(stderr, "Usage: %s [-favorite] (5 white balls) power ball\n", argv[0]);
-	return -1;
+fprintf(stderr, "Usage: %s [-favorite] (5 white balls) power ball\n", argv[0]);
+return -1;
 }
